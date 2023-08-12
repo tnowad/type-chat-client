89eee736cb33211c56e753a176d47a5eca7e4f13
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-12 22:56:46 +0700

    feat(auth): add auth layout with back to home button

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,21 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import NextLink from "next/link";
+
 export default function AuthLayout({
-  children, // will be a page or nested layout
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <section>
-      <div>AuthLayout</div>
+    <section className="flex flex-col justify-center items-center min-h-screen w-full">
+      <Button
+        LinkComponent={NextLink}
+        href={"/"}
+        className="fixed top-4 left-4"
+      >
+        Back to Home
+      </Button>
       {children}
     </section>
   );
