87c8b072f016bb264ce8663a1c8023f76f98f152
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-12 22:59:46 +0700

    feat(verify): add verify page

---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import NextLink from "next/link";
+
+export default function VerifyOTP() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      otp: data.get("otp"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1 }} className="bg-blue-500">
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Verify OTP
+        </Typography>
+        <Typography variant="body2" sx={{ mt: 1 }}>
+          Please enter the verification code sent to your email.
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="otp"
+            label="Verification Code"
+            name="otp"
+            autoComplete="off"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            className="bg-blue-600"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Verify
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link component={NextLink} href="/login" variant="body2">
+                Back to Sign In
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
