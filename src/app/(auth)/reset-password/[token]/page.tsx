aea60a7ac1423c52f25ef76a10e4e574347351ea
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-12 22:59:26 +0700

    feat(reset-password): add reset password page

--- src/app/(auth)/reset-password/[token]/page.tsx
+++ src/app/(auth)/reset-password/[token]/page.tsx
@@ -1,12 +1,80 @@
+"use client";
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
 export default function ResetPassword({
   params,
 }: {
   params: { token: string };
 }) {
+  const { token } = params;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      token,
+      newPassword: data.get("newPassword"),
+      confirmPassword: data.get("confirmPassword"),
+    });
+    // Add logic to handle password reset here
+  };
+
   return (
-    <div>
-      <h1>Reset Password</h1>
-      <p>Token: {params.token}</p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1 }} className="bg-blue-500">
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Reset Password
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="newPassword"
+            label="New Password"
+            type="password"
+            id="newPassword"
+            autoComplete="new-password"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="confirmPassword"
+            label="Confirm Password"
+            type="password"
+            id="confirmPassword"
+            autoComplete="new-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            className="bg-blue-600"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Reset Password
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
