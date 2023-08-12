ceba3ae554cfa4723f8c6ac5a3c68bb57ce626ba
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-12 22:58:45 +0700

    feat(forgot-password): add forgot password page

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,76 @@
+"use client";
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import NextLink from "next/link";
+
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1 }} className="bg-blue-500">
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Forgot Password
+        </Typography>
+        <Typography variant="body2" sx={{ mt: 1 }}>
+          Enter your email address below, and we{"'"}ll send you a link to reset
+          your password.
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            className="bg-blue-600"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Send Reset Link
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link component={NextLink} href="/login" variant="body2">
+                Remember your password? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
