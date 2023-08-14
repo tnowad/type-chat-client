0100de19b467876201007bd4be1c80aeda9858d2
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-14 23:32:56 +0700

    feat(auth): add type for auth api

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,30 +1,41 @@
 import api from "@/utils/api.utils";
+interface ApiResponse<T = object> {
+  success?: boolean;
+  message?: string;
+  data?: T;
+}
+
+interface LoginResponseData {
+  accessToken: string;
+  user: any;
+  refreshToken: string;
+}
 
 const authApi = {
   login: async (values: { email: string; password: string }) =>
-    await api.post(`/api/auth/login`, values),
+    await api.post<ApiResponse<LoginResponseData>>(`/api/auth/login`, values),
 
   register: async (values: {
     firstName: string;
     lastName: string;
     email: string;
     password: string;
-  }) => await api.post(`/api/auth/register`, values),
+  }) => await api.post<ApiResponse>(`/api/auth/register`, values),
 
   verifyOTP: async (values: { email: string; otp: string }) =>
-    await api.post(`/api/auth/verify-otp`, values),
+    await api.post<ApiResponse>(`/api/auth/verify-otp`, values),
 
   sendOTP: async (values: { email: string }) =>
-    await api.post(`/api/auth/send-otp`, values),
+    await api.post<ApiResponse>(`/api/auth/send-otp`, values),
 
   forgetPassword: async (values: { email: string }) =>
-    await api.post(`/api/auth/forget-password`, values),
+    await api.post<ApiResponse>(`/api/auth/forget-password`, values),
 
   resetPassword: async (values: { token: string; newPassword: string }) =>
-    await api.post(`/api/auth/reset-password`, values),
+    await api.post<ApiResponse>(`/api/auth/reset-password`, values),
 
   refreshToken: async (values: { token: string; newPassword: string }) =>
-    await api.post(`/api/auth/refresh-token`, values),
+    await api.post<ApiResponse>(`/api/auth/refresh-token`, values),
 };
 
 export default authApi;
