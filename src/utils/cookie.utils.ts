219b6ff58a0d73f89183095eaf0b14c8aff5a878
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-15 02:34:17 +0700

    feat(utils): add cookie utils

---
+++ src/utils/cookie.utils.ts
@@ -0,0 +1,33 @@
+import Cookies from "js-cookie";
+
+export const accessTokenName = "accessToken" as const;
+export const refreshTokenName = "refreshToken" as const;
+
+export function setAccessToken(token: string) {
+  return Cookies.set(accessTokenName, token);
+}
+
+export function setRefreshToken(token: string) {
+  return Cookies.set(refreshTokenName, token);
+}
+
+export function getAccessToken() {
+  return Cookies.get(accessTokenName);
+}
+
+export function getRefreshToken() {
+  return Cookies.get(refreshTokenName);
+}
+
+export function removeAccessToken() {
+  return Cookies.remove(accessTokenName);
+}
+
+export function removeRefreshToken() {
+  return Cookies.remove(refreshTokenName);
+}
+
+export function clearTokens() {
+  removeAccessToken();
+  removeRefreshToken();
+}
