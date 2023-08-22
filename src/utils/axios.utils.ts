690b0ea59d455f0932a4e98bc7763d35b2594ef2
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-22 12:12:40 +0700

    feat(utils): enhance axios utilities and error handling
    Updated the axios utility module to include more comprehensive error handling and a new function for making API calls. The new 'makeApiCall' function encapsulates the logic for making API requests, handling responses, and managing errors. Additionally, imported the necessary types from Axios for better type checking and documentation.

--- src/utils/axios.utils.ts
+++ src/utils/axios.utils.ts
@@ -1,4 +1,4 @@
-import Axios from "axios";
+import Axios, { AxiosError, AxiosResponse } from "axios";
 
 const axiosInstance = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -8,7 +8,34 @@ const axiosInstance = Axios.create({
   },
 });
 
-export interface ApiResponse<T = object> {
+type ApiCall<T> = (data?: any) => Promise<AxiosResponse<ApiResponse<T>, any>>;
+
+export const makeApiCall = async <T>(
+  apiCall: ApiCall<T>,
+  data?: any
+): Promise<T | null> => {
+  try {
+    const response = await apiCall(data);
+
+    if (response.data && response.data.success) {
+      return response.data.data || null;
+    } else {
+      throw new Error(response.data.message || "Request was not successful.");
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response) {
+        throw new Error(error.response.data.message || "An error occurred.");
+      } else {
+        throw new Error("An error occurred while making the request.");
+      }
+    } else {
+      throw new Error("An unknown error occurred.");
+    }
+  }
+};
+
+export interface ApiResponse<T = any> {
   success?: boolean;
   message?: string;
   data?: T;
