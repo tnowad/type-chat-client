bd59c4407d6a9aca571f5e9951718a7da8e936ed
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-22 13:55:48 +0700

    feat(user): add api call get all users

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,3 +1,9 @@
-const userApi = {};
+import { User } from "@/types/model";
+import axiosInstance, { makeApiCall } from "@/utils/axios.utils";
+
+const userApi = {
+  getAllUsers: async () =>
+    makeApiCall<User[]>(() => axiosInstance.get("/api/users")),
+};
 
 export default userApi;
