eb47f58b5e305f318addd16ccd9660e0ed8e10a8
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-22 14:39:38 +0700

    feat(user): add api fetch user

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -4,6 +4,8 @@ import axiosInstance, { makeApiCall } from "@/utils/axios.utils";
 const userApi = {
   getAllUsers: async () =>
     makeApiCall<User[]>(() => axiosInstance.get("/api/users")),
+  getUserById: async (id: string) =>
+    makeApiCall<User>(() => axiosInstance.get(`/api/users/${id}`)),
 };
 
 export default userApi;
