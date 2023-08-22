c711e8bb9e8fd50073c873e5dea82b9492683694
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-22 19:15:39 +0700

    chore(axios): add default url backend

--- src/utils/axios.utils.ts
+++ src/utils/axios.utils.ts
@@ -13,7 +13,7 @@ const authorization = accessToken
   ? { Authorization: `Bearer ${accessToken}` }
   : {};
 const axiosInstance = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
