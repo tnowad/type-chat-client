2e8388b583a68fcd9f91745e38b0e4d1ffbb40c8
Nguyen Minh Tuan <tnowad@gmail.com>
2023-08-24 13:44:31 +0700

    feat(user): add infor current user and fix atribute

--- src/app/user/[...id]/page.tsx
+++ src/app/user/[...id]/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+import friendApi from "@/apis/friend.api";
 import userApi from "@/apis/user.api";
+import useAuth from "@/hooks/useAuth";
 import { User } from "@/types/model";
-import { Avatar, Button, Paper, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
+import { Avatar } from "@mui/material";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import DoneIcon from "@mui/icons-material/Done";
-import CloseIcon from "@mui/icons-material/Close";
 
 interface UserPageProps {
   params: {
@@ -16,7 +17,8 @@ interface UserPageProps {
 }
 export default function UserPage({ params }: UserPageProps) {
   const { id } = params;
-  const [user, setUser] = useState<User>();
+  const { user } = useAuth();
+  const [userData, setUserData] = useState<User>();
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchUser = async () => {
@@ -24,7 +26,7 @@ export default function UserPage({ params }: UserPageProps) {
         const user = await userApi.getUserById(id);
         console.log(id, user);
         if (user) {
-          setUser(user);
+          setUserData(user);
         }
       } catch (error) {
         toast.error((error as Error).message);
@@ -39,24 +41,33 @@ export default function UserPage({ params }: UserPageProps) {
     return <div>Loading</div>;
   }
 
-  if (!user) {
+  if (!userData) {
     return <div>User not found</div>;
   }
+
+  const handleAddFriend = async () => {
+    try {
+      await friendApi.sendFriendRequest(user?._id!, userData._id);
+      toast.success("Send request successfully");
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  };
 
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="rounded-md shadow-md p-2 w-96">
         <div className="w-full bg-gradient-to-r from-rose-100 to-teal-100 h-32 flex justify-center items-end relative mb-[40px]">
           <Avatar className="!w-20 !h-20 absolute translate-y-1/2 hover:scale-125 duration-150 shadow-md">
-            {user.thumbnail && (
-              <Image src={user.thumbnail} alt={"user thumbnail"} />
+            {userData.avatar && (
+              <Image src={userData.avatar} alt={"user thumbnail"} />
             )}
           </Avatar>
         </div>
         <div className="flex items-center flex-col">
           <h4 className="font-bold text-xl font-sans text-center">
-            {user.firstName + " " + user.lastName}{" "}
-            {user.verified ? (
+            {userData.firstName + " " + userData.lastName}{" "}
+            {userData.verified ? (
               <span className="!text-green-500">
                 <DoneIcon />
               </span>
@@ -66,8 +77,11 @@ export default function UserPage({ params }: UserPageProps) {
               </span>
             )}
           </h4>
-          <p>{user.email}</p>
-          <button className="text-white bg-yellow-400 hover:bg-yellow-500 rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-2/3 font-sans font-extrabold">
+          <p>{userData.email}</p>
+          <button
+            onClick={handleAddFriend}
+            className="text-white bg-yellow-400 hover:bg-yellow-500 rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-2/3 font-sans font-extrabold"
+          >
             Add friend
           </button>
         </div>
